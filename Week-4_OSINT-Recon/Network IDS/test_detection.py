efddfc36f1285f6a61b3765b88aed4f31ff44970
Sristi Dutta <sristidutta02@gmail.com>
2025-09-25 19:59:53 +0530

    Update test_detection.py

--- Week-4_OSINT-Recon/Network IDS/test_detection.py
+++ Week-4_OSINT-Recon/Network IDS/test_detection.py
@@ -1,12 +1,11 @@
-from scapy.all import IP, ICMP, TCP, Ether
+from scapy.all
 from ids_main import detect_from_pcap
 
 def test_icmp_echo_request():
-    # Make a Dummy ICMP packet
-    pkt = Ether()/IP(src="1.2.3.4", dst="5.6.7.8")/ICMP(type=8)
-    assert pkt[ICMP].type == 8
+    pkt = Ether()/IP(src="1.2.3.4", dst="5.6.7.8")/ICMP(type)
+    assert pkt[ICMP]
 
 def test_tcp_syn_flag():
-    pkt = Ether()/IP(src="1.2.3.4", dst="5.6.7.8")/TCP(flags="S")
-    assert pkt[TCP].flags == "S"
+    pkt = Ether()/IP(src="1.2.3.4", dst="5.6.7.8")/TCP(flag)
+    assert pkt[TCP]
     
